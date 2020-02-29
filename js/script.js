95a414b67beba050c616b605e7c8bbe8762fe81d
James Lee <b05502067@ntu.edu.tw>
2020-03-01 02:03:06 +0800

    basic color picker without border detection

--- js/script.js
+++ js/script.js
@@ -31,21 +31,73 @@
 //     return grid;
 // }
 
-function initColorPicker() {
-    var canvasEl = document.getElementById('colorCanvas');
-    var canvasContext = canvasEl.getContext('2d');
+function colorPicker() {
+    var canvasEl = document.getElementById('colorPicker');
+    var context = canvasEl.getContext('2d');
 
-    var image = new Image(300, 260);
-    image.crossOrigin = '';
-    image.onload = () => canvasContext.drawImage(image, 0, 0, image.width, image.height);
-    image.src = "./assets/rgb_triangle.png";
-
-    canvasEl.onclick = function (mouseEvent) {
-        var imgData = canvasContext.getImageData(mouseEvent.offsetX, mouseEvent.offsetY, 1, 1);
-        var rgba = imgData.data;
-
-        alert("rgba(" + rgba[0] + ", " + rgba[1] + ", " + rgba[2] + ", " + rgba[3] + ")");
+    var image = new Image(512, 512);
+    image.onload = function () {
+        context.drawImage(image, 0, 0, image.width, image.height);
     }
+    image.src = "./assets/rgb_hex.png";
+
+    var mouseDown = false;
+    canvasEl.addEventListener('mousedown', function (mouseEvent) {
+        var imgData = context.getImageData(mouseEvent.offsetX, mouseEvent.offsetY, 1, 1);
+        var rgba = imgData.data;
+        if (rgba[0] + rgba[1] + rgba[2] != 0) {
+            mouseDown = true;
+            colorSelected(rgba[0], rgba[1], rgba[2])
+            selectionCircle(mouseEvent.offsetX, mouseEvent.offsetY);
+        }
+
+    });
+
+    canvasEl.addEventListener('mouseup', function (mouseEvent) {
+        mouseDown = false;
+    });
+
+    canvasEl.addEventListener('mousemove', function (mouseEvent) {
+        if (mouseDown) {
+            var imgData = context.getImageData(mouseEvent.offsetX, mouseEvent.offsetY, 1, 1);
+            var rgba = imgData.data;
+            colorSelected(rgba[0], rgba[1], rgba[2]);
+            selectionCircle(mouseEvent.offsetX, mouseEvent.offsetY);
+        }
+    });
 }
 
-initColorPicker();
+
+function colorSelected(r, g, b) {
+    var canvasEL = document.getElementById('colorSelect');
+    var context = canvasEL.getContext('2d');
+    context.clearRect(0, 0, canvasEL.width, canvasEL.height);
+
+    var radius = 64;
+
+    context.beginPath();
+    context.arc(64, 64, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+    context.fill();
+
+    context.font = '20pt Calibri';
+    context.fillText("r : " + r, 4, 170);
+    context.fillText("g : " + g, 1, 205);
+    context.fillText("b : " + b, 0, 240);
+}
+
+function selectionCircle(x, y) {
+    var canvasEL = document.getElementById('selectionCircle');
+    var context = canvasEL.getContext('2d');
+    context.clearRect(0, 0, canvasEL.width, canvasEL.height);
+
+    context.beginPath();
+    context.arc(x, y, 6, 0, 2 * Math.PI, false);
+    context.lineWidth = 3;
+    context.strokeStyle = '#000000';
+    context.stroke();
+}
+
+colorPicker();
+colorSelected(256, 256, 256);
+selectionCircle(256, 256);
